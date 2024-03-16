33cc5b585aa30c681d054d32c4cc37a405724b92
patha2702 <rajendrapatha2702@gmail.com>
2024-03-16 23:09:04 +0530

    temp

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,29 +1,33 @@
-"use client"
+"use client";
 import React, { useState, useEffect } from "react";
 import { HeroSectionBackgroundImages } from "@/constants";
 
 const HeroSection = () => {
   const [backgroundImage, setBackgroundImage] = useState(0);
-  const images = HeroSectionBackgroundImages
-  
+  const images = HeroSectionBackgroundImages;
+
   useEffect(() => {
     const interval = setInterval(() => {
       setBackgroundImage((prevIndex) => (prevIndex + 1) % images.length);
     }, 5000);
-  
+
     return () => clearInterval(interval);
   }, [images.length]);
-  
+
   return (
-    <div className="hero-section relative w-full h-screen overflow-hidden">
+    <section className="hero-section relative w-full h-screen overflow-hidden">
       <div
-        className="absolute top-0 left-0 w-full h-full bg-cover bg-center"
+        className="absolute top-0 left-0 w-full h-full bg-cover bg-center flex justify-center items-center fade"
         style={{
-          backgroundImage: `url(${images[backgroundImage]})`
+          backgroundImage: `url(${images[backgroundImage]})`,
         }}
-      />
-      {/* Your other content here */}
-    </div>
+      >
+        <div className="flex flex-col gap-5 text-center text-white">
+          <span className="capitalize text-7xl font-bold">Welcome to Gokul Krushi Paryatan Kendra</span>
+          <span className="capitalize text-4xl font-semibold">Visit, Enjoy, Experience and return with wonderful experiences</span>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
@@ -13,6 +13,7 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
     },
   },
   plugins: [],
