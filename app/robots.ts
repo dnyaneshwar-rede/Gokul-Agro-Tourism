03b8bc74120a3fb3bb4f37a2a5c954ba45a460db
Dnyaneshwar  Rede <dnyaneshwarrede22@gmail.com>
2024-04-04 02:05:48 +0530

    Update sitemap URL in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/privacy/',
     },
-    sitemap: 'https://gokulagrotourism.co.in/sitemap.xml',
+    sitemap: 'https://gokulagrotourism.co.in/sitemap.ts',
   }
 }
