fd2cd8935fde41aee2ab456a51a1d7337b551ad2
Dnyaneshwar  Rede <dnyaneshwarrede22@gmail.com>
2024-12-29 22:26:43 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,6 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <Hero />
-
       <Video />
       <YoutubeVideo />
       <Activity />
