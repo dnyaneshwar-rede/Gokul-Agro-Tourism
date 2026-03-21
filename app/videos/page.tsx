c548e973b80538b9c7b20f79830528f776da0d52
Dnyaneshwar  Rede <dnyaneshwarrede22@gmail.com>
2026-03-21 21:58:18 +0530

    added privacy policy and improved  the style

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function HomePage() {
   return (
     <main className="container mx-auto px-4 pt-24 min-h-screen py-4">
-      <h1 className="text-4xl font-bold text-center mb-8">Channel Videos</h1>
+      <h1 className="text-4xl font-bold text-center mb-8"> Video Gallary</h1>
       <Suspense fallback={<div className="text-center">Loading videos...</div>}>
         <VideoGrid />
       </Suspense>
